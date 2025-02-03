7883aae37f3caf85d887e3184f3e19e6936bfd0b
Leevance Singh <lsingh.ls629@gmail.com>
2025-02-03 17:48:19 +0530

    backend uploaded

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+//...................................Core Components
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const todoRouter = require("./routes/todoRouter");
+require('dotenv').config();
+const cors = require("cors");
+
+//..................................Middleware's setup
+app.use(cors({
+    origin: '*',
+}));
+app.use(cookieParser());
+app.use(express.static("/public"));
+app.use(express.urlencoded());
+app.use(todoRouter);
+
+
+app.use((req, res, next) => {
+  console.log("----request.method :", req.method);
+  console.log("----request.url :"   , req.url); 
+  next();
+});
+//.............................App initialization
+app.listen(7000,(err,data)=>{
+    if(err)
+        console.log("Server is not connected!!");
+    else{
+        console.log("Server is connected at port 7000");
+    }
+}) 
