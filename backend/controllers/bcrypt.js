8a4ae8701353a8432455aca6b61012845641036b
Vaishali <2004sharmaanshika@gmail.com>
2025-02-03 23:27:16 +0530

    backend

--- backend/controllers/bcrypt.js
+++ backend/controllers/bcrypt.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // becrypt and compare password
 const bcrypt = require("bcrypt");
 async function bcryptPassword(passcode)
@@ -22,28 +21,3 @@ module.exports={
     bcryptPassword,
     comparePassword 
 }
-=======
-// becrypt and compare password
-const bcrypt = require("bcrypt");
-async function bcryptPassword(passcode)
-{
-    try {
-        const salt = await bcrypt.genSalt(10); // Await for salt generation
-        const hash = await bcrypt.hash(passcode, salt); // Await for password hashing
-        return hash;
-    } 
-    catch (err) {
-        console.error('Error hashing password:', err);
-    }
-}
-
-async function comparePassword(passcode,hash){
-    let res;
-    res = await bcrypt.compare(passcode,hash);
-    return res;
-}
-module.exports={
-    bcryptPassword,
-    comparePassword 
-}
->>>>>>> 905a393c54e3e76e6e3e8bc3db8c2645aa1ad8a7
--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,68 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-mongoose
-  .connect(process.env.DB_URL)
-  .then(() => {
-    console.log("mongoDB connected");
-  })
-  .catch((err) => {
-    console.log("MongoDB is not connected:", err);
-  });
-
-const User = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  googleId: {
-    type: String,
-  },
-  mytasks: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "tasks",
-    },
-  ],
-  assignedTasks: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "assignTasks",
-    },
-  ],
-  sections: [
-    {
-      type: String,
-      unique: true,
-      default: ["todo", "inProgress", "completed"],
-    },
-  ],
-});
-
-// Commonly used for validations , checking documents before saving
-User.pre("save", function (next) {
-  //this refres to mongoose document
-  // here we check if both are empty
-  if (!this.password && !this.googleId) {
-    return next(new Error("Either password or googleId must be provided."));
-  }
-  //next works if any of both  field is present
-  next();
-});
-
-const userModel = mongoose.model("users", User);
-
-module.exports = userModel;
-=======
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost:27017/TodoDashboard")
@@ -126,4 +61,3 @@ User.pre("save", function (next) {
 const userModel = mongoose.model("users", User);
 
 module.exports = userModel;
->>>>>>> 905a393c54e3e76e6e3e8bc3db8c2645aa1ad8a7
