15c680cf500a0d83c8f3de04784a86f32f2391cc
shreyavats <shreyavats2407@gmail.com>
2025-02-04 11:18:12 +0530

    chnge

--- backend/controllers/token.js
+++ backend/controllers/token.js
@@ -3,7 +3,7 @@
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
 const secret = process.env.JWT_SECRET;
-console.log(secret) ;
+// console.log(secret) ;
 function makeToken(userObj) {
   // console.log("token",userObj);
   const payload = {
