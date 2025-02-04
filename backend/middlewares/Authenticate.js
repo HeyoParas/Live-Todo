891609e898305e20e6bc02a497953b5403ad8e10
shreyavats <shreyavats2407@gmail.com>
2025-02-04 18:55:30 +0530

    pulling

--- backend/middlewares/Authenticate.js
+++ backend/middlewares/Authenticate.js
@@ -2,17 +2,17 @@ const {getUser} = require("../controllers/token");
 const userModel = require("../models/userSchema");
 const verifyUser =async (req,res,next)=>{
     const userlogin = await getUser(req.cookies.mycookie);
-    const userExists = await userModel.findById({_id:userlogin.id})
-    if(userlogin && userExists)
+    // const userExists = await userModel.findById({_id:userlogin.id})
+    if(userlogin)
         next();
     else
         res.send({message:"Please Login first!!",loginStatus:false});
 }
 const checkLoginStatus = async (req,res)=>{
     const userlogin =  await getUser(req.cookies.mycookie);
-    const userExists = await userModel.findById({_id:userlogin.id})
+    // const userExists = await userModel.findById({_id:userlogin.id})
     console.log(userlogin) ;
-    if(userlogin && userExists){
+    if(userlogin){
         console.log("true");
         res.json({success:true})
     }
--- backend/routes/todoRouter.js
+++ backend/routes/todoRouter.js
@@ -6,17 +6,16 @@ const authController = require("../controllers/AuthController");
 const taskController = require("../controllers/taskController");
 const authMiddleware = require("../middlewares/Authenticate");
 const reportController = require("../controllers/reportController");
-const {authMiddlewareForToken} = require("../controllers/token");
 
-router.get('/getUserData',authMiddlewareForToken,authController.getUserData)
+router.get('/getUserData',authController.getUserData)
 router.get('/auth/checkToken',authMiddleware.checkLoginStatus);
 router.post("/verifyEmail",authController.verifyEmail);
 router.post("/signup",authController.signupUser);
-router.post('/login',authMiddlewareForToken,authMiddleware.isLogin,authController.loginUser); 
-router.post("/addTask",authMiddlewareForToken,authMiddleware.verifyUser,taskController.addTask);
-router.post("/updateTask",authMiddlewareForToken,authMiddleware.verifyUser,taskController.updateTask);
-router.post("/deleteTask",authMiddlewareForToken,authMiddleware.verifyUser,taskController.disableTask)
-router.post("/addSection",authMiddlewareForToken,authMiddleware.verifyUser,taskController.addNewSection);
+router.post('/login',authMiddleware.isLogin,authController.loginUser); 
+router.post("/addTask",authMiddleware.verifyUser,taskController.addTask);
+router.post("/updateTask",authMiddleware.verifyUser,taskController.updateTask);
+router.post("/deleteTask",authMiddleware.verifyUser,taskController.disableTask)
+router.post("/addSection",authMiddleware.verifyUser,taskController.addNewSection);
 
 
 router.get("/report",authMiddleware.verifyUser,reportController.generateReport);
