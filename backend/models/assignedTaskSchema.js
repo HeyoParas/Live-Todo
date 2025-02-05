8488179056adf00f27ded9ffef1daf9aa79c8e87
Leevance <lsingh.ls629@gmail.com>
2025-02-05 17:31:03 +0530

    back to square one

--- backend/models/assignedTaskSchema.js
+++ backend/models/assignedTaskSchema.js
@@ -27,9 +27,9 @@ const assignTasks = new mongoose.Schema({
         max:10,
         default:0
       },
-      accepted:{
-        type:Boolean,
-        default:0
+      status:{
+        type:String,
+        default:"pending"
       }
     },
   ],
