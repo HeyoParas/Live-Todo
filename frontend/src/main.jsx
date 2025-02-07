df5414a6e41e45c624de0de69daf1a8ba205ff9b
Leevance <lsingh.ls629@gmail.com>
2025-02-07 14:36:14 +0530

    report feature updated~

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import TaskAssignment from './sample.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <>
-    <TaskAssignment/>
     <App />
-    </>
 )
