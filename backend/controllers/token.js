8b673d77306c6438a353faa011fe540987a38bc2
shreyavats24 <shreyavats2407@gmail.com>
2025-02-11 12:49:11 +0530

    Comments Added

--- backend/controllers/token.js
+++ backend/controllers/token.js
@@ -4,15 +4,16 @@ const jwt = require("jsonwebtoken");
 require('dotenv').config();
 const secret = process.env.JWT_SECRET;
 // console.log(secret) ;
+// create a token 
 function makeToken(userObj) {
   // console.log("token",userObj);
   const payload = {
     email: userObj.email,
     id: userObj.id,
   };
-  return jwt.sign(payload, secret, { expiresIn: '1h'});
+  return jwt.sign(payload, secret, { expiresIn: '2h'});
 }
-
+// verify token (decrypt)
 function getUser(token) {
   if (!token) return null;
   try {
@@ -23,40 +24,9 @@ function getUser(token) {
   }
 }
 
-const authMiddleware = (req, res, next) => {
-  const token = req.cookies.mycookie; // Get token from cookies
-
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized - No token" });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      if (err.name === "TokenExpiredError") {
-        // Token expired, generate a new one
-        const newToken = jwt.sign(
-          { id: decoded.id, email: decoded.email }, 
-          process.env.JWT_SECRET, 
-          { expiresIn: "1h" } // Set expiration
-        );
-
-        res.cookie("mycookie", newToken, {
-          httpOnly: true,
-          secure: false,  // Set to true in production (HTTPS required)
-          sameSite: "None",
-        });
-      } else {
-        return res.status(403).json({ message: "Invalid Token" });
-      }
-    } 
-
-    next(); // Proceed to the next middleware
-  });
-};
 
 
 module.exports = {
-  authMiddleware,
   makeToken,
   getUser,
 };
