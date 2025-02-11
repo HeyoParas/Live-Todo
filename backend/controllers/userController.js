e1a33cabd3b24748a001ecd99733148cd04c1a48
shreyavats24 <shreyavats2407@gmail.com>
2025-02-11 11:01:33 +0530

    update Profile

---
+++ backend/controllers/userController.js
@@ -0,0 +1,31 @@
+const userModel = require("../models/userSchema");
+const { getUser } = require("../controllers/token");
+const updateProfile = async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const user = await getUser(req.cookies.mycookie);
+    if (!req.file) {
+      return res.json({ message: "No file uploaded",sucess:false });
+    }
+
+    const imagePath = `/uploads/${req.file.filename}`; // Path to save in DB
+
+    // Find the user and update the image path
+    const userData = await userModel.findByIdAndUpdate(
+      user.id, // Assuming you get user ID from authentication
+      { profileImage: imagePath },
+      username,
+      { new: true }
+    );
+
+    res
+      .status(200)
+      .json({ message: "Image uploaded successfully", userData, sucess: true });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server error", error: error.message, sucess: false });
+  }
+};
+module.exports = { updateProfile };
